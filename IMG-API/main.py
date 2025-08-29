e63aa9b6ee4c782d51bf05a21931bd34e5e5fbf4
Ashik Shaji <ashikshaji4u@gmail.com>
2025-08-29 20:14:56 +0530

    5 commit

--- IMG-API/main.py
+++ IMG-API/main.py
@@ -24,6 +24,10 @@ def extract_embedding():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/', methods=['GET'])
+def home():
+    return "API is running!", 200
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
